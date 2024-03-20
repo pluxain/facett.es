01a87bc503d0e55faf7724ee254a883084dadb7c
Pascal Luxain <pascal.luxain@gmail.com>
2024-03-20 23:12:19 +0100

    set up minimal security for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 //Importing project dependancies that we installed earlier
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import { validateEnv } from "./validateEnv";
 
 dotenv.config();
@@ -10,6 +12,8 @@ validateEnv();
 const app = express();
 
 //using the dependancies
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 export { app };
