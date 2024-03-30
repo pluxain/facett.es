289bd2f2df46f2993df467f116ea3f5574247953
Pascal Luxain <pascal.luxain@gmail.com>
2024-03-30 11:37:52 +0100

    populate database mechanism
    - add countries

--- backend/src/routes/countries.ts
+++ backend/src/routes/countries.ts
@@ -2,7 +2,6 @@ import { Request, Response, Router } from "express";
 import { db, schema } from "../database";
 const router = Router();
 
-// TODO: seed the countries table
 router.get("/", (_req: Request, res: Response) => {
   const countries = db.select().from(schema.countries);
   res.send(countries.all());
